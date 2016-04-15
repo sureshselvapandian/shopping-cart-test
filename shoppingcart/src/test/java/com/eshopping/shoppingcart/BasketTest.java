497c1a8df57fb7d347fe8d5be5635c0071fdb06f
selvaps <selvaps@mywork.home>
2016-04-15 21:37:40 +0100

    Add BasketTest and delete the default App.java & test

---
+++ shoppingcart/src/test/java/com/eshopping/shoppingcart/BasketTest.java
@@ -0,0 +1,20 @@
+package com.eshopping.shoppingcart;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Test Basket class
+ * @author suresh
+ *
+ */
+public class BasketTest {
+	
+	/**
+	 * Test item added to basket
+	 */
+	@Test
+	public void testItemAddedToBasket() {
+		assertTrue(true);
+	}
+}
